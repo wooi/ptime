5c802f7611e000592733d239bfa14359dc80dc5a
wooi <sync6288@gmail.com>
2022-12-04 00:12:26 +0800

    format code

--- inser_item.py
+++ inser_item.py
@@ -1,75 +1,77 @@
 #!/usr/bin/python
 # -*- coding=utf-8 -*-
-from xml.etree.ElementTree import ElementTree,Element
 import xml.etree.ElementTree as ET
+
 debug = True
 from pathlib import Path
 
 xml_path = str(Path.cwd()) + '/ptime.xml'
 
-# if debug:
-#     xml_path = "/Users/Wooi/WorkSpace/ptime/pod-ptime/ptime.xml"
-# else :
-#     xml_path = "/usr/share/nginx/html/ptime.xml"
 
 def indent(elem, level=0):
-    i = "\n" + level*"\t"
+    i = "\n" + level * "\t"
     if len(elem):
         if not elem.text or not elem.text.strip():
             elem.text = i + "\t"
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
-            indent(elem, level+1)
+            indent(elem, level + 1)
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
     else:
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
+
 def read_xml(in_path):
-    '''读取并解析xml文件
+    """读取并解析xml文件
        in_path: xml路径
-       return: ElementTree'''
-    ET.register_namespace('atom','http://www.w3.org/2005/Atom/')
-    ET.register_namespace('content','http://purl.org/rss/1.0/modules/content/')
-    ET.register_namespace('dc','http://purl.org/dc/elements/1.1/')
-    ET.register_namespace('fireside','http://fireside.fm/modules/rss/fireside')   
-    ET.register_namespace('googleplay','http://www.google.com/schemas/play-podcasts/1.0')     
-    ET.register_namespace('itunes','http://www.itunes.com/dtds/podcast-1.0.dtd')
-    ET.register_namespace('rdf','http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-    ET.register_namespace('sy','ttp://purl.org/rss/1.0/modules/syndication/')
+       return: ElementTree"""
+    ET.register_namespace('atom', 'http://www.w3.org/2005/Atom/')
+    ET.register_namespace('content', 'http://purl.org/rss/1.0/modules/content/')
+    ET.register_namespace('dc', 'http://purl.org/dc/elements/1.1/')
+    ET.register_namespace('fireside', 'http://fireside.fm/modules/rss/fireside')
+    ET.register_namespace('googleplay', 'http://www.google.com/schemas/play-podcasts/1.0')
+    ET.register_namespace('itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd')
+    ET.register_namespace('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+    ET.register_namespace('sy', 'ttp://purl.org/rss/1.0/modules/syndication/')
     tree = ET.parse(in_path)
     return tree
 
+
 def create_node(tag, property_map, content):
-    '''新造一个节点
+    """新造一个节点
        tag:节点标签
        property_map:属性及属性值map
        content: 节点闭合标签里的文本内容
-       return 新节点'''
+       return 新节点"""
     element = ET.Element(tag, property_map)
     element.text = content
     return element
 
+
 def create_p_node(tag):
-    '''新造一个节点
+    """新造一个节点
        tag:节点标签
        property_map:属性及属性值map
        content: 节点闭合标签里的文本内容
-       return 新节点'''
+       return 新节点"""
     element = ET.Element(tag)
     return element
 
+
 def create_c_node(tag, content):
-    '''新造一个节点
+    """新造一个节点
        tag:节点标签
        property_map:属性及属性值map
        content: 节点闭合标签里的文本内容
-       return 新节点'''
+       return 新节点"""
     element = ET.Element(tag)
     element.text = content
-    return element    
+    return element
+
+
 #   <item>
 #      <title>笑死朕 第422集 - 熱血東遊記1</title>
 #      <link>http://www.passiontimes.hk/prog/5/422</link>
@@ -87,30 +89,30 @@ def create_c_node(tag, content):
 #      <content:encoded></content:encoded>
 #      <itunes:summary></itunes:summary>
 #   </item>
-def write2xml(title_str,link_str,guid_str,pubDate_str,author_str,download_link):
-    #1. 读取xml文件
+def write2xml(title_str, link_str, guid_str, pubDate_str, author_str, download_link):
+    # 1. 读取xml文件
     tree = read_xml(xml_path)
     nodes = tree.getroot().find("channel")
     item = create_p_node("item")
 
-    title = create_c_node("title",title_str)
-    link = create_c_node("link",link_str)
+    title = create_c_node("title", title_str)
+    link = create_c_node("link", link_str)
     # guid = create_c_node("guid",guid_str)
     guid = ET.Element("guid")
-    guid.attrib = {"isPermaLink":"false"}
+    guid.attrib = {"isPermaLink": "false"}
     guid.text = guid_str
-    pubDate = create_c_node("pubDate",pubDate_str)
-    author = create_c_node("author",author_str)
+    pubDate = create_c_node("pubDate", pubDate_str)
+    author = create_c_node("author", author_str)
     enclosure = ET.Element("enclosure")
-    enclosure.attrib = {"url":download_link,"length":"31279627","type":"audio/mpeg"}
-    itunes_episodeType = create_c_node("itunes:episodeType","full")
-    itunes_author = create_c_node("itunes:author","PT")
-    itunes_subtitle = create_c_node("itunes:subtitle",author_str)
-    itunes_duration = create_c_node("itunes:duration","30:00")
-    itunes_explicit = create_c_node("itunes:explicit","no")
+    enclosure.attrib = {"url": download_link, "length": "31279627", "type": "audio/mpeg"}
+    itunes_episodeType = create_c_node("itunes:episodeType", "full")
+    itunes_author = create_c_node("itunes:author", "PT")
+    itunes_subtitle = create_c_node("itunes:subtitle", author_str)
+    itunes_duration = create_c_node("itunes:duration", "30:00")
+    itunes_explicit = create_c_node("itunes:explicit", "no")
     itunes_image = ET.Element("itunes:image")
-    itunes_image.attrib = {"href":"http://www.passiontimes.hk/4.0/favicons/android-chrome-192x192.png"}
-    description = create_c_node("description",author_str)
+    itunes_image.attrib = {"href": "http://www.passiontimes.hk/4.0/favicons/android-chrome-192x192.png"}
+    description = create_c_node("description", author_str)
     content_encoded = ET.Element("content:encoded")
     itunes_summary = ET.Element("itunes:summary")
 
@@ -130,11 +132,11 @@ def write2xml(title_str,link_str,guid_str,pubDate_str,author_str,download_link):
     item.append(content_encoded)
     item.append(itunes_summary)
 
-    nodes.insert(18,item)
+    nodes.insert(18, item)
     indent(nodes)
     # ET.dump(nodes)
-    tree.write(xml_path, encoding="utf-8",xml_declaration=True)
-    print('inser-suc')
+    tree.write(xml_path, encoding="utf-8", xml_declaration=True)
+    print('Insert,Download Link:', download_link, end='\n')
 
 
 # channel_map = {"5":"405","85":"731"}
@@ -148,8 +150,6 @@ def get_cid():
         if cid[0] not in channel_map:
             channel_map[cid[0]] = cid[1]
         else:
-            if channel_map[cid[0]]< cid[1]:
+            if channel_map[cid[0]] < cid[1]:
                 channel_map[cid[0]] = cid[1]
-    return channel_map         
-
-# print get_cid()   
+    return channel_map
--- podcast_task.py
+++ podcast_task.py
@@ -1,69 +1,65 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from bs4 import BeautifulSoup
-import requests
-import json
-from xml.etree.ElementTree import ElementTree,Element
-from xml_praser import *
 import re
+
+import requests
+from bs4 import BeautifulSoup
+
 from inser_item import *
 
-home_index = ["5","84","sitdowncomedy"]
-# home_index = ["84"]
+home_index = ["5", "84", "sitdowncomedy"]
 
 page_link = 'http://www.passiontimes.hk'
 
-def get_detail(urls,prog):
-    for url in urls:
-        channel_link = url
-        channel_response = requests.get(channel_link, timeout=60)
+
+def get_detail(url_address, prog):
+    for url in url_address:
+        channel_response = requests.get(url, timeout=60)
         channel_html = channel_response.content.decode("utf8", "ignore").encode("utf", "ignore")
         channel_content = BeautifulSoup(channel_html, "html.parser", from_encoding='utf-8')
 
-        title = channel_content.find('meta',attrs={'property': 'og:title'})['content']
-        infos = channel_content.find('h4',attrs={'class': 'byline'})
-        author =  infos.find('span',attrs={'class':'host'}).a.string
-        date =  infos.find('time',attrs={'class':'published'})['datetime']
-        print(str(title.encode('utf-8'))+str(author.encode('utf-8'))+str(date.encode('utf-8')))
-       
+        detail_title = channel_content.find('meta', attrs={'property': 'og:title'})['content']
+        detail_info = channel_content.find('h4', attrs={'class': 'byline'})
+        author = detail_info.find('span', attrs={'class': 'host'}).a.string
+        date = detail_info.find('time', attrs={'class': 'published'})['datetime']
+        print(str(detail_title) + str(author) + str(date), end='\n')
+
         if u'黃洋達' not in author:
-            print(str(title.encode('utf-8'))+str(url)+' not 黃洋達 pass')
+            print('PASS, The author does not include 黃洋達 ', end='\n')
             continue
 
-        #downlaod link
+        # download link
         scripts = channel_content.find_all("script")
         for script in scripts:
-            content =  script.string
+            content = script.string
             if content is None:
                 continue
             if '.mp3' not in content:
                 continue
-            lines =  content.split(';')
-            index = 0
+            lines = content.split(';')
+            s_index = 0
             for line in lines:
                 if '.mp3' not in line:
                     continue
-                download_link =re.findall(r'"(.*?)(?<!\\)"', line)[0]
-                index +=1
-                title_str = title+'-'+str(index)
-                # guid = prog+","+url.split('/')[-1]+","+index
-                guid = '{},{},{}'.format(prog,url.split('/')[-1],index)
-                write2xml(title_str=title_str,link_str=url,guid_str=guid,pubDate_str=date,author_str=author,download_link=download_link)    
-                print(download_link)
+                download_link = re.findall(r'"(.*?)(?<!\\)"', line)[0]
+                s_index += 1
+                title_str = detail_title + '-' + str(s_index)
+                guid = '{},{},{}'.format(prog, url.split('/')[-1], s_index)
+                write2xml(title_str=title_str, link_str=url, guid_str=guid, pubDate_str=date, author_str=author,
+                          download_link=download_link)
 
 
+latest_episode = get_cid()
+limit_keys = latest_episode.keys()
+for channel_id in home_index:
+    latest_index = latest_episode[channel_id]
+    print('===================Channel========================')
+    print('channelId:', channel_id, ' Latest:', latest_index, end='\n')
+    channel_link = page_link + "/prog/{}/".format(channel_id)
 
-limit_id =get_cid()
-print(limit_id)
-# for index in home_index:
-for index in home_index:
-    limit = limit_id[index]
-    print('limit:',limit)
-    home_link = page_link+"/prog/{}/".format(index)
-    
-    print(home_link)
-    page_response = requests.get(home_link, timeout=60)
+    print(channel_link, end='\n')
+    page_response = requests.get(channel_link, timeout=60)
     html = page_response.content
     html2 = html.decode("utf8", "ignore").encode("utf", "ignore")
     page_content = BeautifulSoup(html2, "html.parser", from_encoding='utf-8')
@@ -72,22 +68,13 @@ for index in home_index:
     for module in module_array:
         title = module.find("h4")
         if title is not None:
-            url_index= title.a.get("href")
+            url_index = title.a.get("href")
             last_index = url_index.split('/')[-1]
-            # print 'current id:',last_index
-            if int(last_index)>int(limit):
-                urls.append(page_link+url_index)
-    # print urls
-    if len(urls) >0:    
-        get_detail(urls=urls,prog=index)
+            if int(last_index) > int(latest_index):
+                urls.append(page_link + url_index)
+    if len(urls) > 0:
+        get_detail(url_address=urls, prog=channel_id)
     else:
-        print('not update')
-
-
-
-
-
-
-
-
+        print('Not update', end='\n')
+    print('===================End========================\n')
 
